db0433b6f99a1f65676efc8770b081f596e54276
erskinenyongesawanami <erskineviper@gmail.com>
2025-06-27 06:26:21 +0300

    Search function to get cocktails by ingredient

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
   searchBtn.addEventListener("click", handleSearch);
   randomBtn.addEventListener("click", getRandomDrink);
 });
+
+function handleSearch() {
+  const ingredient = document.getElementById("ingredient-input").value.trim();
+  if (!ingredient) return;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.drinks) {
+        renderCocktails(data.drinks);
+      } else {
+        document.getElementById("cocktail-container").innerHTML = "No cocktails found.";
+      }
+    });
+}
